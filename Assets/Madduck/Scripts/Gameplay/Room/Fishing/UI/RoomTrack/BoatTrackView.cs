06325cd8e59d3ba9a3721d28df7f158259656aa2
chain3132 <148683922+chain3132@users.noreply.github.com>
2025-10-01 13:27:55 +0700

    More animation BoatUI and add asset InGameUI

--- Assets/Madduck/Scripts/Gameplay/Room/Fishing/UI/RoomTrack/BoatTrackView.cs
+++ Assets/Madduck/Scripts/Gameplay/Room/Fishing/UI/RoomTrack/BoatTrackView.cs
@@ -8,7 +8,9 @@ namespace Madduck.Room
     public class BoatTrackView : MonoBehaviour
     {
         [SerializeField] private Image icon;
-        
+        [SerializeField] private TweenSettings moveBoatTweenSettings;
+        [SerializeField] private TweenSettings rotateBoatTweenSettings;
+        [SerializeField] private Vector3 rotateAngle = new(0f, 0f, 10f);
         private Sequence _boatTrackSequence;
         
         #region SetUp
@@ -22,13 +24,18 @@ namespace Madduck.Room
             var boatUI = (RectTransform)transform;
             var roomPosInParent = (Vector2)boatUI.parent.InverseTransformPoint(targetWorldPos);
             _boatTrackSequence.Stop();
-            _boatTrackSequence = Sequence.Create()
-                .Group(Tween.UIAnchoredPosition(boatUI, roomPosInParent, 2f, Ease.Linear))
-                .Group(Tween.LocalRotation(boatUI, Quaternion.Euler(0f, 0f, 10f), 2f, Ease.InOutSine));
-            _boatTrackSequence.OnComplete(() =>
+            var moveTween = Tween.UIAnchoredPosition(boatUI, roomPosInParent, moveBoatTweenSettings);
+
+            var rotateTween = Tween.LocalRotation(boatUI, Quaternion.Euler(rotateAngle), rotateBoatTweenSettings);
+            rotateTween.OnComplete(() =>
             {
-                boatUI.rotation = Quaternion.identity;
+                Tween.LocalRotation(boatUI, Quaternion.Euler(-rotateAngle), rotateBoatTweenSettings)
+                    .OnComplete(() => Tween.LocalRotation(boatUI, Quaternion.identity, rotateBoatTweenSettings));
+                    
             });
+            _boatTrackSequence = Sequence.Create()
+                .Group(moveTween)
+                .Group(rotateTween);
         }
         #endregion
     }
